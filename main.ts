1a74f599401464b3fc4eff5e361423a139cbff3a
SilviaFabara <107445419+SilviaFabara@users.noreply.github.com>
2023-06-12 13:58:08 -0600

    SEGMENTE EL CODIGO . UNO CODIGO PARA CONTROL OTRO PARA ACTUADOR

--- main.ts
+++ main.ts
@@ -5,19 +5,5 @@ radio.onReceivedNumber(function (receivedNumber) {
         servos.P0.setAngle(90)
     }
 })
-input.onGesture(Gesture.ScreenUp, function () {
-    radio.sendNumber(0)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(1)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-})
 radio.setGroup(1)
 servos.P0.setAngle(90)
