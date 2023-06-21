03fc74588c1070dbbfc3576610760d2862035abe
SilviaFabara <107445419+SilviaFabara@users.noreply.github.com>
2023-06-21 17:40:49 -0500

    QUIERO AGREGARLE DATA LOGGER PARA REGISTRAR DATOS DEL MOVIMIENTO DE LA CASA

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 def on_received_number(receivedNumber):
+    global estadox, estadoy
     if receivedNumber == 1:
         servos.P0.set_angle(30)
+        estadox = 1
     elif receivedNumber == 0:
         servos.P0.set_angle(90)
+        estadoy = 0
 radio.on_received_number(on_received_number)
 
-def on_gesture_screen_up():
-    radio.send_number(0)
-    basic.clear_screen()
-input.on_gesture(Gesture.SCREEN_UP, on_gesture_screen_up)
+def on_button_pressed_a():
+    global logging
+    logging = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_gesture_tilt_left():
-    radio.send_number(1)
-    basic.show_leds("""
-        # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-    """)
-input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+def on_button_pressed_b():
+    radio.set_group(1)
+    if input.is_gesture(Gesture.SCREEN_UP):
+        radio.send_number(0)
+    elif input.is_gesture(Gesture.LOGO_DOWN):
+        radio.send_number(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
+estadoy = 0
+estadox = 0
+logging = False
 radio.set_group(1)
 servos.P0.set_angle(90)
+logging = False
+datalogger.set_column_titles("X", "Y")
+
+def on_every_interval():
+    if logging:
+        datalogger.log(datalogger.create_cv("X", estadox),
+            datalogger.create_cv("Y", estadoy))
+loops.every_interval(1000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,9 +1,30 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         servos.P0.setAngle(30)
+        estadox = 1
     } else if (receivedNumber == 0) {
         servos.P0.setAngle(90)
+        estadoy = 0
     }
 })
+input.onButtonPressed(Button.A, function () {
+    logging = true
+})
+let estadoy = 0
+let estadox = 0
+let logging = false
 radio.setGroup(1)
 servos.P0.setAngle(90)
+logging = false
+datalogger.setColumnTitles(
+"X",
+"Y"
+)
+loops.everyInterval(1000, function () {
+    if (logging) {
+        datalogger.log(
+        datalogger.createCV("X", estadox),
+        datalogger.createCV("Y", estadoy)
+        )
+    }
+})
